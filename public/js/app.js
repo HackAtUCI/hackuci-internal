53f60989ca12b1675cf87831bda15ed7d8362ea8
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2016-05-17 21:27:27 -0700

    ngRoute added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-var hackuci = angular.module('hackuci', []);
+var hackuci = angular.module('hackuci', ['ngRoute']);
+
+hackuci.config(
+    function($routeProvider) {
+        $routeProvider
+
+            //home page
+            .when('/', {
+                templateUrl : 'views/home.html',
+                controller  : 'mainController'
+            })
+
+            //registration route
+            .when('/register', {
+                templateUrl : 'views/register.html',
+                controller  : 'mainController'
+            });
+    }
+
+
+
+)
+
+
+hackuci.controller('mainController', function($scope) {
+
+})
